c02a0819073786c1ee0311768062dd69418c7e27
Active--mcc <1014374431@qq.com>
2019-08-12 10:13:10 +0800

    week2

---
+++ src/main/java/com/macongcong/example/demo/RedisMacongcongWeek2Application.java
@@ -0,0 +1,13 @@
+package com.macongcong.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMacongcongWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisMacongcongWeek2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/macongcong/example/demo/RedisMacongcongWeek2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.macongcong.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisMacongcongWeek2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
